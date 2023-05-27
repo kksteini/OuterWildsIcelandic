baaf2867f5521c617c46b9c2ab1980e58145f239
kksteini <77672665+kksteini@users.noreply.github.com>
2023-05-27 21:24:44 +0000

    Add files via upload

---
+++ ILocalizationAPI.cs
@@ -0,0 +1,12 @@
+using OWML.ModHelper;
+using System;
+
+namespace IcelandicTranslation
+{
+    public interface ILocalizationAPI
+    {
+        void RegisterLanguage(ModBehaviour mod, string name, string translationPath);
+        void AddLanguageFont(ModBehaviour mod, string name, string assetBundlePath, string fontPath);
+        void AddLanguageFixer(string name, Func<string, string> fixer);
+    }
+}
---
+++ IcelandicTranslation.cs
@@ -0,0 +1,30 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+
+namespace IcelandicTranslation
+{
+    public class IcelandicTranslation : ModBehaviour
+    {
+        private void Awake()
+        {
+            // You won't be able to access OWML's mod helper in Awake.
+            // So you probably don't want to do anything here.
+            // Use Start() instead.
+        }
+
+        private void Start()
+        {
+            // Starting here, you'll have access to OWML's mod helper.
+            ModHelper.Console.WriteLine($"My mod {nameof(IcelandicTranslation)} is loaded!", MessageType.Success);
+
+            var api = ModHelper.Interaction.TryGetModApi<ILocalizationAPI>("xen.LocalizationUtility");
+            api.RegisterLanguage(this, "Icelandic", "assets/Translation.xml");
+            // Example of accessing game code.
+            LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
+            {
+                if (loadScene != OWScene.SolarSystem) return;
+                ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+            };
+        }
+    }
+}
