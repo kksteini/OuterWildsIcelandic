43a3890dfee6179ae1bbea76a18b28eb485250dc
kksteini <steini@kosmosogkaos.is>
2023-06-22 12:45:18 +0000

    Lítilsháttar lagfæringar

--- assets/find_ritvills.rb
+++ assets/find_ritvills.rb
@@ -5,12 +5,14 @@ $f = File.open("uniquer.txt", "r").readlines.collect{|v| v.chomp}.sort
 puts "Loading complete"
 
 $reasons = []
+$processed = 0
 
 def process_element(element)
   # If the name of this element ends with 'entry', print its text
   if element.name.end_with?('entry') || element.name.end_with?('Entry')
+    $processed += 1
     if not element.elements['value'].nil? then
-      clean_text = element.elements['value'].to_s[7..-9].gsub(/&lt.*?&gt;/, '').gsub(/[0123456789—“”…;:()"';,\.!?]/, "").downcase
+      clean_text = element.elements['value'].to_s[7..-9].gsub(/&lt.*?&gt;/, '').gsub(/[ 0123456789—“”…;:()"';,\.!?]/, "").downcase
       reasons = clean_text.split(" ").select{|v| !$f.include? v}
 
       unless reasons.length.zero? then 
@@ -23,7 +25,8 @@ def process_element(element)
         puts "Reasons so far: #{$reasons}"
       end
     end
-  end
+    puts "Have processed #{$processed} of around 3000" if $processed % 100 == 0
+ end
 
   # Recursively process each child of this element
   element.elements.each do |child|
