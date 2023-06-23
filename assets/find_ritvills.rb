f7feaa1210ed484b18f2fc978ea655266f47e481
kksteini <steini@kosmosogkaos.is>
2023-06-23 08:53:25 +0000

    Mjög snögg lagfæring meðan ég bíð eftir öðru í vinnunni

--- assets/find_ritvills.rb
+++ assets/find_ritvills.rb
@@ -12,7 +12,7 @@ def process_element(element)
   if element.name.end_with?('entry') || element.name.end_with?('Entry')
     $processed += 1
     if not element.elements['value'].nil? then
-      clean_text = element.elements['value'].to_s[7..-9].gsub(/&lt.*?&gt;/, '').gsub(/[ 0123456789—“”…;:()"';,\.!?]/, "").downcase
+      clean_text = element.elements['value'].to_s[7..-9].gsub('\n', " ").gsub(/&lt.*?&gt;/, '').gsub(/[ 0123456789—\\“”…;:()"';,\.!?]/, "").downcase
       reasons = clean_text.split(" ").select{|v| !$f.include? v}
 
       unless reasons.length.zero? then 
