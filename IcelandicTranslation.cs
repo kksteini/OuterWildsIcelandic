0d73d3c3c945b239c310f070386d8cf70a58278d
kksteini <steini@kosmosogkaos.is>
2023-06-27 08:49:47 +0000

    Smá cleanup

--- IcelandicTranslation.cs
+++ IcelandicTranslation.cs
@@ -15,16 +15,10 @@ namespace IcelandicTranslation
         private void Start()
         {
             // Starting here, you'll have access to OWML's mod helper.
-            ModHelper.Console.WriteLine($"My mod {nameof(IcelandicTranslation)} is loaded!", MessageType.Success);
+            ModHelper.Console.WriteLine($"{nameof(IcelandicTranslation)} is loaded!", MessageType.Success);
 
             var api = ModHelper.Interaction.TryGetModApi<ILocalizationAPI>("xen.LocalizationUtility");
             api.RegisterLanguage(this, "Icelandic", "assets/Translation.xml");
-            // Example of accessing game code.
-            LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
-            {
-                if (loadScene != OWScene.SolarSystem) return;
-                ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
-            };
         }
     }
 }
